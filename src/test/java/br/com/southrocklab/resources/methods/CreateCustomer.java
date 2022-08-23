6ca3875466e3646b45872b0b985b413d104086ca
Roni Miguel <ronimiguel@gmail.com>
2022-08-23 19:24:33 -0300

    add: Map birthday maior e altera last name

--- src/test/java/br/com/southrocklab/resources/methods/CreateCustomer.java
+++ src/test/java/br/com/southrocklab/resources/methods/CreateCustomer.java
@@ -16,4 +16,20 @@ public class CreateCustomer {
         return customer;
     }
 
+    public Map<String, Object> ComBirthDateMaiorQueHoje() {
+        Map<String, Object> customer = new HashMap<String, Object>();
+        customer.put("name", customerData.getName());
+        customer.put("lastName", customerData.getLastName());
+        customer.put("birthDate", customerData.getBirthDateMaiorQueHoje());
+        return customer;
+    }
+
+    public Map<String, Object> AlteraLastName() {
+        Map<String, Object> customer = new HashMap<String, Object>();
+        customer.put("name", customerData.getName());
+        customer.put("lastName", customerData.getLastNameAlterado());
+        customer.put("birthDate", customerData.getBirthDate());
+        return customer;
+    }
+
 }

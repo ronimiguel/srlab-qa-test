7329e4c03a8942a0381c3a938a38b8b2dcfcdb57
Roni Miguel <ronimiguel@gmail.com>
2022-08-23 19:45:05 -0300

    add: Map to save new Card

---
+++ src/test/java/br/com/southrocklab/resources/methods/CreateCard.java
@@ -0,0 +1,22 @@
+package br.com.southrocklab.resources.methods;
+
+import br.com.southrocklab.resources.tdm.InfoCard;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CreateCard {
+    InfoCard cardData = new InfoCard();
+
+    public Map<String, Object> salvarNovo(int customerId){
+        Map<String, Object> card = new HashMap<String, Object>();
+        card.put("brand", cardData.getBrand());
+        card.put("cvc", cardData.getCvc());
+        card.put("expirationMoth", cardData.getExpMonth());
+        card.put("expirationYear", cardData.getExpYear());
+        card.put("holderName", cardData.getHolderName());
+        card.put("number", cardData.getCardNumber());
+        card.put("customerId", customerId);
+        return card;
+    }
+}

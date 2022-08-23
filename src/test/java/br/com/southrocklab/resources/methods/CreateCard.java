e3e7fee20cbb73903e3b61776b0d4ba70ac8a705
Roni Miguel <ronimiguel@gmail.com>
2022-08-23 19:58:59 -0300

    add: Map com expMonthMaior que 12

--- src/test/java/br/com/southrocklab/resources/methods/CreateCard.java
+++ src/test/java/br/com/southrocklab/resources/methods/CreateCard.java
@@ -32,4 +32,16 @@ public class CreateCard {
         return card;
     }
 
+    public Map<String, Object> comExpMonthMaior(int customerId){
+        Map<String, Object> card = new HashMap<String, Object>();
+        card.put("brand", cardData.getBrand());
+        card.put("cvc", cardData.getCvc());
+        card.put("expirationMoth", cardData.getExpMonthMaior());
+        card.put("expirationYear", cardData.getExpYear());
+        card.put("holderName", cardData.getHolderName());
+        card.put("number", cardData.getCardNumber());
+        card.put("customerId", customerId);
+        return card;
+    }
+
 }

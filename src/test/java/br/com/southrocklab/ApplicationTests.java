3c5edddf0e7061c186b8233478ab306c2d9bfe8c
Roni Miguel <ronimiguel@gmail.com>
2022-08-23 16:32:52 -0300

    add: Basic Test Setup

--- src/test/java/br/com/southrocklab/ApplicationTests.java
+++ src/test/java/br/com/southrocklab/ApplicationTests.java
@@ -1,11 +1,15 @@
 package br.com.southrocklab;
 
 import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
 import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static br.com.southrocklab.resources.core.PathConstants.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -16,6 +20,19 @@ public abstract class ApplicationTests {
 
     @Before
     public void setUp() {
-        RestAssured.port = porta;
+        RestAssured.baseURI = APP_BASE_URL;
+//        RestAssured.port = porta; // PORTA aleatoria
+        RestAssured.port = APP_PORT; // PORTA 9090
+        RestAssured.basePath = APP_BASE_PATH;
+
+        RequestSpecBuilder reqBuilder = new RequestSpecBuilder();
+        reqBuilder.setContentType(APP_CONTENT_TYPE);
+        RestAssured.requestSpecification = reqBuilder.build();
+
+        ResponseSpecBuilder resBuilder = new ResponseSpecBuilder();
+        RestAssured.responseSpecification = resBuilder.build();
+
+        // Mostra log().all() em caso de falhas
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
     }
 }
